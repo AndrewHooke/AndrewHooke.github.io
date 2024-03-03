86dc90f3964647936c40db1f9c0a43bd10683ac7
AndrewHooke <161822263+AndrewHooke@users.noreply.github.com>
2024-03-03 18:19:04 +0300

    Add files via upload

--- java/script.js
+++ java/script.js
@@ -1,4 +1,16 @@
+emailjs.init("OBFr8KmgnoC_loyAX"); // Этот ID будет вашим виртуальным почтальоном
+
 function toggleMenu() {
   var navLinks = document.querySelector('.nav-links');
   navLinks.classList.toggle('show');
 }
+
+document.getElementById('form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Не обновляйте страницу, пока сообщение не отправлено
+    emailjs.sendForm('service_duar265', 'template_1bfdxrg', this)
+        .then(function() {
+            alert('Сообщение успешно отправлено!');
+        }, function(error) {
+            alert('Ошибка отправки: ' + error);
+        });
+});
